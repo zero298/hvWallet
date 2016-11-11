72a55c3c9954ee787ab1e13fc2e51769b4c1f81d
Stephen K <zero298@gmail.com>
2016-11-10 19:20:17 -0500

    Housekeeping
    + Fleshed out the README
    + Fixed card deletion handling
    + Updated gh-pages
    + Removed old code

--- README.md
+++ README.md
@@ -1,5 +1,60 @@
 # hvWallet
 
-Web component to mimic iOS wallet.
+## About
+
+hvWallet is a simple AngularJS module that provides a responsive card container structure similar to iOS's Wallet and Reminders apps.
+
+## Installation
+
+**Manual:** download [the latest release](https://github.com/zero298/hvWallet/releases).
+
+**Automatic:** Install hvWallet using [Bower](https://bower.io/).</p>
+
+```bash
+bower install hv-wallet
+```
+
+## Usage
+
+Inclue hvWallet in your code.
+
+```js
+<script src="hvWallet.js"></script>
+<link rel="stylesheet" href="hvWallet.css">
+```
+
+Inject hvWallet as a dependency to your application.
+
+```js
+var app = angular.module("myApp", ["hvWallet"]);
+```
+
+Create some elements that use hvWallet.
+
+```html
+<ul hv-wallet="">
+    <li hv-wallet-card="">
+        <p>Hello World!</p>
+    </li>
+</ul>
+```
+
+## Demo
 
 See demo on the [hvWallet demo page](https://zero298.github.io/hvWallet/).
+
+## Share
+
+Please feel free to send a pull request.
+
+## License
+
+**The MIT License (MIT)**
+
+Copyright (c) 2016 Stephen K
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- src/hvWallet.controller.js
+++ src/hvWallet.controller.js
@@ -8,9 +8,15 @@
         COLLAPSED_CONTAINER_HEIGHT = 100;
 
     function offsetWithin(arr, allocatedSpace) {
+        var curr = 0,
+            offsetSpace,
+            increment;
         allocatedSpace = (allocatedSpace || COLLAPSED_CONTAINER_HEIGHT);
+        offsetSpace = (100 - allocatedSpace);
+        increment = ((1 / arr.length) * allocatedSpace);
         arr.forEach(function (currCard, idx, allCards) {
-            currCard.css("top", (((idx / allCards.length) * allocatedSpace) + (100 - allocatedSpace)) + "%");
+            currCard.css("top", (curr + offsetSpace) + "%");
+            curr += increment;
         });
     }
 
@@ -22,38 +28,37 @@
     WalletCtrl.prototype = {
         registerWalletCard: function (card) {
             this.cards.push(card);
-            offsetWithin(this.cards);
+            this.render();
         },
         unregisterWalletCard: function (card) {
             this.cards.splice(this.cards.indexOf(card), 1);
-            offsetWithin(this.cards);
+            this.focus(card);
         },
         focus: function (card) {
-            var vm = this;
-            if (this.focussedCard === card) {
-                this.focussedCard = null;
-            } else {
-                this.focussedCard = card;
-            }
-            this.$window.requestAnimationFrame(function () {
-                vm.render();
-            });
+            this.focussedCard = (this.focussedCard === card ? null : card);
+            this.render();
         },
         render: function () {
-            var cardsClone = this.cards.slice(),
-                card = this.focussedCard;
+            var cards = this.cards,
+                cardsClone = this.cards.slice(),
+                focussedCard = this.focussedCard;
 
-            if (card) {
-                cardsClone.splice(this.cards.indexOf(card), 1);
-                card.addClass("expanded");
-                card.css("top", "");
-                offsetWithin(cardsClone, EXPANDED_CONTAINER_HEIGHT);
-            } else {
-                this.cards.forEach(function (currCard) {
-                    currCard.removeClass("expanded");
-                });
-                offsetWithin(cardsClone);
+            function realRender() {
+                if (focussedCard) {
+                    cardsClone.splice(cards.indexOf(focussedCard), 1);
+                    focussedCard.addClass("expanded");
+                    focussedCard.css("top", "");
+                    offsetWithin(cardsClone, EXPANDED_CONTAINER_HEIGHT);
+                } else {
+                    cards.forEach(function (currCard) {
+                        currCard[0].scrollTop = 0;
+                        currCard.removeClass("expanded");
+                    });
+                    offsetWithin(cardsClone);
+                }
             }
+
+            this.$window.requestAnimationFrame(realRender);
         }
     };
     WalletCtrl.$inject = [
